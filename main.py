78f2085b2fd9b73ec83e897c4ae79791dd1fe909
HeyLexicore <110418351+HeyLexicore@users.noreply.github.com>
2022-08-02 17:42:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import requests
+from PIL import Image
+import random
+from io import BytesIO
+
+x = 16
+y = 16
+
+def make(t):
+
+    url = "http://ciaccodavi.de/qbdp/acg/alchemymod.php?id=" +str(random.randint(1000000000, 9999999999))
+    img = requests.get(url, stream=True)
+
+
+    i = Image.open(BytesIO(img.content))
+
+
+
+
+
+    function = open("C:/Users/fjodo_e97m5hu/curseforge/minecraft/Instances/min/saves/New World/datapacks/DEMON/data/demon/functions/circle"+str(t)+".mcfunction","w")
+    function.truncate(0)
+
+    for k in range(0,int(i.width)):
+        for j in range(0,int(i.height)):
+            if i.getpixel((k,j)) == (255,255,255):
+                #function.write('particle dust 1.0 0.0 0.0 1.0 '+ "^"+str((k/(128/x))-(x/2)) +" ^ ^" +str((j/(128/y))-(y/2)) + " 0.0 0.0 0.0 0.1 1\n")
+                #function.write('particle lava ' + "^" + str((k / (128 / x)) - (x / 2)) + " ^ ^" + str((j / (128 / y)) - (y / 2)) + " 0.0 0.0 0.0 0.1 1\n")
+                
+
+
+
+
+
+for i in range(0,4):
+    make(i)
